41905ae750a00c297a620e1cb6322cc090f434f3
Mark Hobbs <markhobbs91@gmail.com>
2022-05-01 10:15:35 +0100

    Updating MaterialModel class

--- material_model.py
+++ material_model.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class MaterialModel():
@@ -28,16 +30,30 @@ class LinearElasticityPerfectPlasticity():
         strain_y = self.stress_y / self.E
 
         for i in range(len(strain)):
-            if strain < strain_y:
-                tmp = self.E * strain
-            elif strain > strain_y:
+            if strain[i] < strain_y:
+                tmp = self.E * strain[i]
+            elif strain[i] > strain_y:
                 tmp = self.stress_y
             stress.append(tmp)
 
         return stress
 
-    def plot_stress_strain():
-        pass
+    def stress_strain_graph(self, strain):
+        stress = self.calculate_stress(strain)
+        plt.plot(strain, stress)
+        plt.title("Stress-strain graph")
+        plt.xlabel("Strain $\epsilon$")
+        plt.ylabel("Stress $\sigma$")
+
+    def generate_synthetic_data(self, strain, s_noise, n_data_points):
+        """
+        The noise in the stress measurements is a normal distribution with a
+        zero mean and a standard deviation of s_noise
+        """
+        strain_data = np.random.choice(strain, n_data_points)
+        stress_data = (self.calculate_stress(strain_data)
+                       + (s_noise * np.random.normal(size=n_data_points)))
+        return strain_data, stress_data
 
 
 class LinearElasticityLinearHardening(MaterialModel):
